552c471d5a430a2c1c126e7d11b73546f2bb3929
Arthur Langereis <puurtuur@me.com>
2018-08-14 01:18:32 +0200

    core game present

--- src/ld42.ts
+++ src/ld42.ts
@@ -2,12 +2,105 @@
  * Blarbs - a Ludum Dare 42 Entry by Arthur Langereis (@zenmumbler)
  */
 
-import { Arena, ARENA_W, ARENA_H, Stick, Direction } from "./arena";
+import { Arena, ARENA_W, ARENA_H, Stick, Direction, Quad } from "./arena";
+import * as dom from "@zenmumbler/mini-dom";
+import { SFX, Sound } from "./sfx";
 
-const enum Actor {
+const enum ActorType {
 	Player,
 	Mipmip,
-	Slurf
+}
+
+interface Actor {
+	type: ActorType;
+	elem: HTMLElement;
+	x: number;
+	y: number;
+	nextDir: Direction | undefined;
+}
+
+function makeBoxElem(quad: Quad) {
+	const [x, y] = arena.topLeftOfQuad(quad);
+	const [left, top] = view.pixelCoordForPosition(x, y);
+	const el = document.createElement("div");
+	el.className = "box";
+	el.style.left = `${left}px`;
+	el.style.top = `${top}px`;
+	root.appendChild(el);
+	return el;
+}
+
+function makeActor(type: ActorType): Actor {
+	const actor: Actor = {
+		elem: document.createElement("div"),
+		type,
+		x: 0,
+		y: 0,
+		nextDir: undefined
+	};
+	actor.elem.className = "hidden " + (type === ActorType.Player ? "blarb" : "mipmip");
+	root.appendChild(actor.elem);
+	return actor;
+}
+
+function spawnActor(actor: Actor, x: number, y: number) {
+	actor.x = x;
+	actor.y = y;
+	actor.elem.style.left = `${actor.x * GRID_TILE_DIM}px`;
+	actor.elem.style.top = `${actor.y * GRID_TILE_DIM}px`;
+	actor.elem.classList.remove("hidden");
+	actor.elem.classList.add("actor");
+}
+
+function moveActor(actor: Actor, dir: Direction) {
+	if (! arena.canMove(actor.x, actor.y, dir)) {
+		return;
+	}
+	const stickCoord = arena.stickCoordAffectingDirection(actor.x, actor.y, dir);
+	if (stickCoord) {
+		if (dir < Direction.UP) {
+			if (arena.stickOrientationAt(stickCoord[0], stickCoord[1]) === Stick.Vert) {
+				arena.setStickOrientationAt(stickCoord[0], stickCoord[1], Stick.Horiz);
+				const elem = view.stickElemAt(stickCoord[0], stickCoord[1]);
+				if (elem) {
+					elem.classList.remove("vert");
+				}
+
+				const boxCheckDir = (stickCoord[1] <= actor.y) ? Direction.UP : Direction.DOWN;
+				const boxQuad = arena.quadRelativeToStickAt(stickCoord[0], stickCoord[1], boxCheckDir);
+				if (arena.quadIsBox(boxQuad) && actor.type === ActorType.Player) {
+					arena.freezeQuad(boxQuad);
+					view.freezeQuad(boxQuad);
+					makeBoxElem(boxQuad);
+					sound.play(SFX.Box);
+				}
+			}
+		}
+		else {
+			if (arena.stickOrientationAt(stickCoord[0], stickCoord[1]) === Stick.Horiz) {
+				arena.setStickOrientationAt(stickCoord[0], stickCoord[1], Stick.Vert);
+				const elem = view.stickElemAt(stickCoord[0], stickCoord[1]);
+				if (elem) {
+					elem.classList.add("vert");
+				}
+
+				const boxCheckDir = (stickCoord[0] <= actor.x) ? Direction.LEFT : Direction.RIGHT;
+				const boxQuad = arena.quadRelativeToStickAt(stickCoord[0], stickCoord[1], boxCheckDir);
+				if (arena.quadIsBox(boxQuad) && actor.type === ActorType.Player) {
+					arena.freezeQuad(boxQuad);
+					view.freezeQuad(boxQuad);
+					makeBoxElem(boxQuad);
+					sound.play(SFX.Box);
+				}
+			}
+		}
+	}
+
+	const { dx, dy } = movementForDirection(dir);
+	actor.x += dx;
+	actor.y += dy;
+	actor.elem.style.left = `${actor.x * GRID_TILE_DIM}px`;
+	actor.elem.style.top = `${actor.y * GRID_TILE_DIM}px`;
 }
 
 const GRID_TILE_DIM = 57;
@@ -52,15 +145,15 @@ class ArenaView {
 		return this.sticks[(y * ARENA_W) + x];
 	}
 
+	freezeQuad(quad: Quad) {
+		const t = this.stickElemAt(quad.top[0], quad.top[1]); t && (t.classList.add("frozen"));
+		const l = this.stickElemAt(quad.left[0], quad.left[1]); l && (l.classList.add("frozen"));
+		const r = this.stickElemAt(quad.right[0], quad.right[1]); r && (r.classList.add("frozen"));
+		const b = this.stickElemAt(quad.bottom[0], quad.bottom[1]); b && (b.classList.add("frozen"));
+	}
+
 	pixelCoordForPosition(x: number, y: number) {
 		return [x * GRID_TILE_DIM, y * GRID_TILE_DIM];
-	}
-
-	horizontalMotion(x1: number, x2: number, y: number, actor: Actor) {
-
-	}
-	verticalMotion(x: number, y1: number, y2: number, actor: Actor) {
-
 	}
 }
 
@@ -82,72 +175,149 @@ function movementForDirection(dir: Direction) {
 	}
 }
 
+interface Rhythm {
+	lastMove: number;
+	interval: number;
+}
+
+let root: HTMLElement;
+let arena: Arena;
+let view: ArenaView;
+let sound: Sound;
+let blarb: Actor;
+let mipmip: Actor;
+let rhythm: Rhythm;
+let mode: "load" | "intro" | "game" | "lose" | "win" = "load";
+
+function show(sel: string) {
+	dom.$1(sel).classList.remove("hidden");
+}
+function hide(sel: string) {
+	dom.$1(sel).classList.add("hidden");
+}
+
+function setMode(newMode: "load" | "intro" | "game" | "lose" | "win") {
+	if (newMode === "game") {
+		rhythm.lastMove = performance.now();
+		hide(".intro");
+		sound.stopMusic();
+		sound.startMusic(false);
+		spawnActor(blarb, 3, 0);
+		spawnActor(mipmip, ARENA_W - 2, 7);
+	}
+	if (newMode === "intro") {
+		sound.stopMusic();
+		sound.startMusic(true);
+	}
+	mode = newMode;
+}
+
+export function start(fullscreen: boolean) {
+	sound.activate();
+	hide(".launch");
+	show(".status");
+	show(".arena");
+	setMode("intro");
+	if (fullscreen) {
+		// const el = dom.$1(".game") as any;
+		// (el.requestFullscreen || el.webkitRequestFullscreen || el.webkitRequestFullScreen || el.mozRequestFullScreen).call(el);
+	}
+}
+
+function nextMipDir() {
+	const dx = blarb.x - mipmip.x;
+	const dy = blarb.y - mipmip.y;
+
+	const chase = Math.random() < 0.65;
+	const r = Math.random();
+	if (r < 0.5) {
+		if (chase) {
+			return dx > 0 ? Direction.RIGHT : Direction.LEFT;
+		}
+		return Math.random() < 0.5 ? Direction.RIGHT : Direction.LEFT;
+	}
+	else {
+		if (chase) {
+			return dy > 0 ? Direction.DOWN : Direction.UP;
+		}
+		return Math.random() < 0.5 ? Direction.DOWN : Direction.UP;
+	}
+}
+
+let controlDir: Direction | undefined;
+
 export function main() {
-	const root = document.querySelector(".arena")! as HTMLElement;
-	const arena = new Arena();
-	const view = new ArenaView(arena, root);
+	root = dom.$1(".arena");
+	rhythm = { lastMove: 0, interval: 0.46875 * 1000 / 2 };
+	sound = new Sound();
+	arena = new Arena();
+	view = new ArenaView(arena, root);
 	(window as any).view = view;
 
-	let blarbX = 0;
-	let blarbY = 0;
-	const blarb = document.createElement("div");
-	blarb.className = "actor blarb";
-	root.appendChild(blarb);
+	blarb = makeActor(ActorType.Player);
+	mipmip = makeActor(ActorType.Mipmip);
 
-	const move = (dir: Direction) => {
-		if (! arena.canMove(blarbX, blarbY, dir)) {
-			return;
-		}
-		const stickCoord = arena.stickCoordAffectingDirection(blarbX, blarbY, dir);
-		if (stickCoord) {
-			if (dir < Direction.UP) {
-				if (arena.stickOrientationAt(stickCoord[0], stickCoord[1]) === Stick.Vert) {
-					arena.setStickOrientationAt(stickCoord[0], stickCoord[1], Stick.Horiz);
-					const elem = view.stickElemAt(stickCoord[0], stickCoord[1]);
-					if (elem) {
-						elem.classList.remove("vert");
-					}
-
-					const boxCheckDir = (stickCoord[1] <= blarbY) ? Direction.UP : Direction.DOWN;
-					const boxQuad = arena.quadRelativeToStickAt(stickCoord[0], stickCoord[1], boxCheckDir);
-					if (arena.quadIsBox(boxQuad)) {
-						console.info("BOX! ^v");
-						arena.freezeQuad(boxQuad);
-					}
+	const next = () => {
+		if (mode === "game") {
+			const now = performance.now();
+			const nextMove = rhythm.lastMove + rhythm.interval;
+			if (now > nextMove) {
+				rhythm.lastMove = now;
+				const nextBlarbDir = controlDir === undefined ? blarb.nextDir : controlDir;
+				if (nextBlarbDir !== undefined) {
+					moveActor(blarb, nextBlarbDir);
+					blarb.nextDir = undefined;
 				}
-			}
-			else {
-				if (arena.stickOrientationAt(stickCoord[0], stickCoord[1]) === Stick.Horiz) {
-
-					arena.setStickOrientationAt(stickCoord[0], stickCoord[1], Stick.Vert);
-					const elem = view.stickElemAt(stickCoord[0], stickCoord[1]);
-					if (elem) {
-						elem.classList.add("vert");
-					}
-
-					const boxCheckDir = (stickCoord[0] <= blarbX) ? Direction.LEFT : Direction.RIGHT;
-					const boxQuad = arena.quadRelativeToStickAt(stickCoord[0], stickCoord[1], boxCheckDir);
-					if (arena.quadIsBox(boxQuad)) {
-						console.info("BOX! <>");
-						arena.freezeQuad(boxQuad);
-					}
+				mipmip.nextDir = nextMipDir();
+				if (mipmip.nextDir !== undefined) {
+					moveActor(mipmip, mipmip.nextDir);
+				}
+				if (mipmip.x === blarb.x && mipmip.y === blarb.y) {
+					sound.play(SFX.Die);
+					spawnActor(blarb, 3, 0);
+					spawnActor(mipmip, ARENA_W - 2, 7);
 				}
 			}
 		}
 
-		const { dx, dy } = movementForDirection(dir);
-		blarbX += dx;
-		blarbY += dy;
-		blarb.style.left = `${blarbX * GRID_TILE_DIM}px`;
-		blarb.style.top = `${blarbY * GRID_TILE_DIM}px`;
+		window.requestAnimationFrame(next);
 	};
+
+	window.requestAnimationFrame(next);
 
 	document.onkeydown = (evt) => {
-		switch (evt.keyCode) {
-			case Key.UP: move(Direction.UP); break;
-			case Key.DOWN: move(Direction.DOWN); break;
-			case Key.LEFT: move(Direction.LEFT); break;
-			case Key.RIGHT: move(Direction.RIGHT); break;
+		if (! (evt.metaKey || evt.ctrlKey)) {
+			evt.preventDefault();
+		}
+		if (evt.repeat) {
+			return;
+		}
+		if (mode === "intro") {
+			if (evt.keyCode === 32) {
+				setMode("game");
+			}
+		}
+		else if (mode === "game") {
+			switch (evt.keyCode) {
+				case Key.UP: blarb.nextDir = controlDir = Direction.UP; break;
+				case Key.DOWN: blarb.nextDir = controlDir = Direction.DOWN; break;
+				case Key.LEFT: blarb.nextDir = controlDir = Direction.LEFT; break;
+				case Key.RIGHT: blarb.nextDir = controlDir = Direction.RIGHT; break;
+			}
 		}
 	};
+
+	document.onkeyup = (evt) => {
+		if (mode === "game") {
+			switch (evt.keyCode) {
+				case Key.UP: if (controlDir === Direction.UP) { controlDir = undefined; } break;
+				case Key.DOWN: if (controlDir === Direction.DOWN) { controlDir = undefined; } break;
+				case Key.LEFT: if (controlDir === Direction.LEFT) { controlDir = undefined; } break;
+				case Key.RIGHT: if (controlDir === Direction.RIGHT) { controlDir = undefined; } break;
+			}
+		}
+	};
+
+	return sound.loadedPromise; // yep
 }
+
